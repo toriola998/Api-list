e9f7c1db37648a3991232e8ec1be36b563049be7
Toriola Faidat <faaedah@gmail.com>
2022-02-09 13:08:56 +0100

    added header component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <template>
-    <h1>Hello World</h1>
+    <Header />
 </template>
 
 <script>
+import Header from './components/Header.vue'
 
 
 export default {
-  name: 'App',
-  components: {
-   
-  }
+    name: 'App',
+    components: {
+        Header
+        }
 }
 </script>
 
 <style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    background-color: #fff;
+    height: 100vh;
+    width: 100%;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="header">
+        <h1>List of Free Api</h1>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+    .header{
+        background-color: #e5e5e5;
+        color: #000;
+        text-align: center;
+        height: 8rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
+
