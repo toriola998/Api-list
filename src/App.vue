2dbec49741c26b1d56bd0553fa331bd359cddba3
Toriola Faidat <faaedah@gmail.com>
2022-02-09 14:05:22 +0100

    added input elements

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
     <Header />
+    <input placeholder="Search API..." v-model="title" @input="getInput()"/>
+    <label for="Category">Choose a Category</label>
+    <select id="category" name="category">
+        <option value="">Cat</option>
+    </select>
 </template>
 
 <script>
 import Header from './components/Header.vue'
 
-
 export default {
     name: 'App',
     components: {
         Header
+    },
+    data() {
+        return {
+           title: '', 
         }
+    },
+    methods: {
+        getInput(){
+            //this.title = title
+           console.log(this.title) 
+        }
+    }
 }
 </script>
 
